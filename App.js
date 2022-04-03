d6543e25cb54c94ef181c9b6ee377c5e9dcd81f8
hicham-elkamouni <hichamelkamouni.dev@gmail.com>
2022-04-03 16:37:17 +0000

    first push

---
+++ App.js
@@ -0,0 +1,11 @@
+import { StatusBar } from "expo-status-bar";
+import { StyleSheet, Text, View } from "react-native";
+import { HomeScreen } from "./screens/HomeScreen";
+
+export default function App() {
+  return (
+    <HomeScreen />
+  );
+}
+
+
---
+++ screens/HomeScreen.js
@@ -0,0 +1,10 @@
+import { View, Text, SafeAreaView } from 'react-native'
+import React from 'react'
+
+export const HomeScreen = () => {
+  return (
+    <SafeAreaView style={{ flex : 1}}>
+      <Text>HOME SCREEN</Text>
+    </SafeAreaView>
+  )
+}
