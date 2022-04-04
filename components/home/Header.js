5377b2a16b4ccfac9c0595a0544b88e7c14c4657
hicham-elkamouni <hichamelkamouni.dev@gmail.com>
2022-04-04 22:50:55 +0000

    trying to impement the header

--- components/home/Header.js
+++ components/home/Header.js
@@ -1,18 +1,62 @@
-import { View, Text, Image, StyleSheet } from 'react-native'
-import React from 'react'
+import { View, Text, Image, StyleSheet, TouchableOpacity } from "react-native";
+import React from "react";
 
 export const Header = () => {
   return (
-    <View>
-      <Image style={styles.logo} source={require('../../assets/instagram_logo.png')} />	
+    <View style={styles.container}>
+      <TouchableOpacity>
+        <Image
+          style={styles.logo}
+          source={require("../../assets/instagram_logo.png")}
+        />
+      </TouchableOpacity>
+
+      <View style={styles.iconsContainer}>
+        <TouchableOpacity>
+          <Image
+            style={styles.icon}
+            source={require("../../assets/plus_logo.png")}
+          />
+        </TouchableOpacity>
+        <TouchableOpacity>
+          <Image
+            style={styles.icon}
+            source={require("../../assets/heart.png")}
+          />
+        </TouchableOpacity>
+        <TouchableOpacity>
+          <Image
+            style={styles.icon}
+            source={require("../../assets/send.png")}
+          />
+        </TouchableOpacity>
+      </View>
     </View>
-  )
-}
+  );
+};
 
 const styles = StyleSheet.create({
+  container : {
+    justifyContent: 'space-between',
+    alignItems : 'center',
+    flexDirection: 'row',
+    marginHorizontal: 20,
+  },
+
+  iconsContainer : {
+    flexDirection : 'row',
+  },
+
   logo: {
     width: 100,
     height: 50,
-    resizeMode: 'contain',
+    resizeMode: "contain",
   },
-})
+
+  icon: {
+    width: 30,
+    height: 30,
+    marginLeft : 10,
+    resizeMode: "contain",
+  }
+});
