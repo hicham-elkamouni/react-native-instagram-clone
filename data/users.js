9935e7f0f607d1fe0d5245488ba802de036d8d69
hicham-elkamouni <hichamelkamouni.dev@gmail.com>
2022-04-24 21:37:59 +0000

    changing data

---
+++ data/users.js
@@ -0,0 +1,32 @@
+export const USERS = [
+    {
+        user: "JasminSmith",
+        image: "https://randomuser.me/api/portraits/med/women/10.jpg",
+        // isOnline: true,
+    },
+    {
+        user: "yamashsimi",
+        image: "https://randomuser.me/api/portraits/med/men/12.jpg",
+        // isOnline: true,
+    },
+    {
+        user: "JohnDoe",
+        image: "https://randomuser.me/api/portraits/med/women/22.jpg",
+        // isOnline: true,
+    },
+    {
+        user: "silvester",
+        image: "https://randomuser.me/api/portraits/med/men/23.jpg",
+        // isOnline: false,
+    },
+    {
+        user: "ananamakoki",
+        image: "https://randomuser.me/api/portraits/med/women/22.jpg",
+        // isOnline: true
+    },
+    {
+        user: "fatisisco",
+        image: "https://randomuser.me/api/portraits/med/women/2.jpg",
+        // isOnline: true
+    }
+]
